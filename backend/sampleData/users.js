ff18999037d806ded7ddb346395db4e88a9b0428
Amaankhan377 <amaankhn02@gmail.com>
2026-03-05 15:57:41 +0530

    Add sample user data for testing compatibility engine

---
+++ backend/sampleData/users.js
@@ -0,0 +1,22 @@
+const users = [
+{
+name: "Rahul",
+sleepTime: 23,
+cleanlinessLevel: 4,
+smoking: false,
+studyHours: 3,
+noiseTolerance: 2,
+personalityType: "Introvert"
+},
+{
+name: "Priya",
+sleepTime: 23,
+cleanlinessLevel: 4,
+smoking: false,
+studyHours: 4,
+noiseTolerance: 2,
+personalityType: "Ambivert"
+}
+];
+
+module.exports = users;
